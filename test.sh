d53c7abdeec27f3857658500668d43b49cb9ef59
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2025-09-24 22:53:15 +0200

    Adjusted build

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 # Assumes an existing virtualenv at .venv
 source .venv/bin/activate
-cd docker
-python -m unittest helloapp.tests.test -vvv
-python -m pytest helloapp/tests/
+cd docker/helloapp
+python -m unittest tests.test -vvv
+python -m pytest tests/
