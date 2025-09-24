66371f3cf832076b924d77a3a57f2f0cecbd001c
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2025-09-24 22:01:20 +0200

    Adjusted helloapp folder to docker, restructured webapp folder and added Dockerfile

---
+++ docker/helloapp/tests/test_app.py
@@ -0,0 +1,16 @@
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app 
+import pytest
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.text == "<p>Hello, World!</p>"
--- helloapp/test.py
+++
@@ -1,6 +0,0 @@
-from helloapp.app import hello_world
-import unittest
-
-class TestHello(unittest.TestCase):
-    def test_hello(self):
-        self.assertIn("Hello", hello_world(), "Endpoint is not polite enough.")
